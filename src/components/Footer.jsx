3e2fb0569846890cfee3e95b02581435a950fca2
PattarapongNoah435 <container.sasiphat@gmail.com>
2025-09-07 13:08:43 +1000

    first commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer>&#169; {`${new Date().getFullYear()} - Digital Midnight`}</footer>
+    <footer>&#169; {`${new Date().getFullYear()} - Noah Pattarapong Chinwatcharaset`}</footer>
   );
 };
 
